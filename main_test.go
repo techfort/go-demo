3d4d5f44b8905e5178728b43af4528f3c0987465
Joe Minichino <joe.minichino@gmail.com>
2019-10-25 17:08:00 +0100

    purposely failing test to test jenkins email

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestMain(t *testing.T) {
 	t.Run("tests Messages func", func(t *testing.T) {
-		expected := "hi there, i love go!"
+		expected := "hi there, i love goo!"
 		if Message("go") != expected {
 			t.Errorf("error: expected %s\n got %s", expected, Message("go"))
 		}
